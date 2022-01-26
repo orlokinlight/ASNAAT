37436baaa08a4ff9ccdc5bc1665f2d3830e7e655
Rsera1 <45517636+Rsera1@users.noreply.github.com>
2022-01-26 09:57:32 -0500

    Delete db.bat

--- Protocols/db.bat
+++
@@ -1,7 +0,0 @@
-@if (@This==@IsBatch) @then
-wscript //E:JScript "%~dpnx0" %1
-exit /b
-@end
-var se = decodeURI(WScript.arguments(0).split("db-open:")[1]);
-WScript.CreateObject("WScript.Shell").Run("cmd /C " + "C:\\Users\\robse\\Documents\\Extremist-Application\\DB_Browser\\DB_Browser.exe" + " " + se,0);
-WScript.Quit(0);
